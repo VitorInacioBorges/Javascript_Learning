202c8e8bdad875af2564ca3bbda0f37e7df45e4b
Vitor Inacio Borges <vitorinacioborges@gmail.com>
2025-05-12 13:32:55 -0300

    Logical Operators Update

--- Javascript_Learning/scripts/logical_operators.js
+++ Javascript_Learning/scripts/logical_operators.js
@@ -9,11 +9,26 @@ Three types:
 FOR EXAMPLE:
 */
 
-let number = 2;
+let number = 3;
 let message;
+
 if ((number < 2) || (number > 2)){
     message = ("It's not 2!");
 } else {
     message = ("It's 2!");
+}
+console.log(message);
+
+if ((number < 5) && (number > 0)){
+    message = ("It's between 0 and 5!");
+} else {
+    message = ("It's NOT between 0 and 5!");
+}
+console.log(message);
+
+if (!((number % 2) == 0)) {
+    message = ("It's NOT divisible by 2!");
+} else {
+    message = ("It's divisible by 2!");
 }
 console.log(message);
