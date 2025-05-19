c6ae89c95ec6778943ec9aba213ca20a272ad6fb
Vitor Inacio Borges <vitorinacioborges@gmail.com>
2025-05-19 11:01:16 -0300

    Guessing Game

---
+++ Javascript_Learning/scripts/guessing_game.js
@@ -0,0 +1,22 @@
+const MIN = 1, MAX = 100;
+let guess, running = true;
+let answer = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+
+for(let attempts = 0; running; attempts++){
+    guess = window.prompt(`Please guess a number between ${MIN} and ${MAX}:`);
+    guess = Number(guess);
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    } else if (guess < MIN || guess > MAX){
+        window.alert("Please enter a valid number");
+    } else {
+        if(guess < answer){
+            window.alert("HIGHER!!!");
+        } else if(guess > answer){
+            window.alert("LOWER!!!");
+        } else {
+            window.alert(`CORRECT! It took you ${attempts} attempts.`);
+            running = false;
+        }
+    }
+}
